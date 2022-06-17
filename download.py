ca6d2d3cc9c6a96f4d1b70fec2fc711d030ac701
Volundr AI <trevor_wieland@mac.com>
2022-06-17 21:54:31 +0900

    Fixed predownload script

--- download.py
+++ download.py
@@ -4,6 +4,7 @@
 # In this example: A Huggingface BERT model
 
 from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
